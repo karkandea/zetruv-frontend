0479f3f0256ec256f4aa02acdd23fa9c01522a8b
Arkan <98953892+karkandea@users.noreply.github.com>
2026-08-30 15:22:48 +0700

    Compose redesigned homepage sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,28 @@
 import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import ServiceCategories from '../components/ServiceCategories'
+import PopularAndRecent from '../components/PopularAndRecent'
 import FlashSale from '../components/FlashSale'
-import TrendingGames from '../components/TrendingGames'
+import JockeyGames from '../components/JockeyGames'
+import Merchandise from '../components/Merchandise'
 import SupportChat from '../components/SupportChat'
 import Footer from '../components/Footer'
 import { homepageMock } from '../data/mockData'
 import { getHomepageData } from '../services/homeService'
+
+function mergeHomepageData(result = {}) {
+  return {
+    ...homepageMock,
+    ...result,
+    serviceCategories: result.serviceCategories ?? homepageMock.serviceCategories,
+    popularGames: result.popularGames ?? homepageMock.popularGames,
+    recentPurchases: result.recentPurchases ?? homepageMock.recentPurchases,
+    flashSale: result.flashSale ?? homepageMock.flashSale,
+    jockeyGames: result.jockeyGames ?? result.trending ?? homepageMock.jockeyGames,
+    merchandise: result.merchandise ?? homepageMock.merchandise,
+  }
+}
 
 export default function HomePage() {
   const [data, setData] = useState(homepageMock)
@@ -14,7 +30,7 @@ export default function HomePage() {
   useEffect(() => {
     let active = true
     getHomepageData().then((result) => {
-      if (active) setData(result)
+      if (active) setData(mergeHomepageData(result))
     })
     return () => { active = false }
   }, [])
@@ -23,9 +39,12 @@ export default function HomePage() {
     <div className="site-shell">
       <Navbar />
       <main>
-        <Hero registeredUsers={data.registeredUsers} />
+        <Hero />
+        <ServiceCategories items={data.serviceCategories} />
+        <PopularAndRecent popular={data.popularGames} recent={data.recentPurchases} />
         <FlashSale items={data.flashSale} countdown={data.flashSaleEndsAt} />
-        <TrendingGames items={data.trending} />
+        <JockeyGames items={data.jockeyGames} />
+        <Merchandise items={data.merchandise} />
       </main>
       <Footer />
       <SupportChat />
