5ad355389ae69ba46c22990d51f243929afc2ea0
Arkan <98953892+karkandea@users.noreply.github.com>
2026-08-30 22:17:05 +0700

    Route via-login search page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import SearchPage from './pages/SearchPage'
 export default function App() {
   const path = window.location.pathname.replace(/\/+$/, '') || '/'
 
+  if (path === '/search/login' || path === '/categories/login') {
+    return <SearchPage mode="login" />
+  }
+
   if (path === '/search' || path === '/categories') {
     return <SearchPage />
   }
