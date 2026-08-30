1e967a6f418851e3b84b1e3bfecd82ebc9b41c02
Arkan <98953892+karkandea@users.noreply.github.com>
2026-08-30 22:02:06 +0700

    Add search page Figma assets

---
+++ src/data/searchAssets.js
@@ -0,0 +1,14 @@
+export const searchAssets = {
+  categoryPlayerId: 'https://www.figma.com/api/mcp/asset/e0d2a714-d4cb-425f-bf57-eb5dd6ead4c4.png',
+  categoryLogin: 'https://www.figma.com/api/mcp/asset/0beac9f0-c9c3-40e0-a5e6-92bdc3d6be05.png',
+  categoryItems: 'https://www.figma.com/api/mcp/asset/53245c53-4a40-46a6-8852-a4fc7209ecd2.png',
+  categoryAccounts: 'https://www.figma.com/api/mcp/asset/72e1f5a8-be4d-4b10-a6e5-ad71af7894a3.png',
+  categoryMerchandise: 'https://www.figma.com/api/mcp/asset/ecacf286-1ef7-4f79-b636-122d205133b4.png',
+  mobileLegends: 'https://www.figma.com/api/mcp/asset/822a60b1-0464-4961-862c-2d9669b8457b.png',
+  pubgMobile: 'https://www.figma.com/api/mcp/asset/f99e996b-ac37-4c92-98ad-d35eded22e2b.png',
+  valorant: 'https://www.figma.com/api/mcp/asset/3e9dc241-5ce8-48c9-afd8-58c4525a1e2e.png',
+  genshinImpact: 'https://www.figma.com/api/mcp/asset/2a4514f0-0938-4c45-947a-907b04945cd1.png',
+  callOfDutyMobile: 'https://www.figma.com/api/mcp/asset/8b95789a-a050-49dc-9743-4daa587bf022.png',
+  starRail: 'https://www.figma.com/api/mcp/asset/b5cc11b0-67fe-4137-b6f1-2bf3843772fe.png',
+  searchIcon: 'https://www.figma.com/api/mcp/asset/5d5b0f4e-b436-4a95-a29d-993e3b5fc38b.svg',
+}
