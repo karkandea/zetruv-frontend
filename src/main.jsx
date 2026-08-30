e1ac76c125ff1953ac9030ab6a4b7ac72989a426
Arkan <98953892+karkandea@users.noreply.github.com>
2026-08-30 15:25:19 +0700

    Load redesigned homepage styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
+import './styles/redesign-a.css'
+import './styles/redesign-b.css'
+import './styles/redesign-c.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
