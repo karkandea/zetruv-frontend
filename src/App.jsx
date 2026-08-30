439609257f810ca8b579e880aa5faf8261e2bef4
Arkan <98953892+karkandea@users.noreply.github.com>
2026-08-30 22:03:32 +0700

    Add search page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
 
 export default function App() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+
+  if (path === '/search' || path === '/categories') {
+    return <SearchPage />
+  }
+
   return <HomePage />
 }
