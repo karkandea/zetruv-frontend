b59f2d6c37cc93cdb7673096fdc1cabca70d6c7c
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-03 10:36:16 +0700

    feat: apply redesigned admin visual layer

--- admin/main.jsx
+++ admin/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AdminApp from '../src/admin/AdminApp'
 import '../src/admin/admin.css'
+import '../src/admin/admin-reference.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
