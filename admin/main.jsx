96a947a99d9bb8399eed7f792755cde0d0d94518
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-03 10:48:25 +0700

    feat: hydrate admin UI with Lucide icons

--- admin/main.jsx
+++ admin/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AdminApp from '../src/admin/AdminApp'
+import LucideIconLayer from '../src/admin/LucideIconLayer'
 import '../src/admin/admin.css'
 import '../src/admin/admin-reference.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AdminApp />
+    <LucideIconLayer />
   </React.StrictMode>,
 )
