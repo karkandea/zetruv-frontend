159f503921057dcb54b97a3bc35fbc6e29579389
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-06 21:23:43 +0700

    fix: match footer structure to exact Figma node

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,54 @@
 import { assets } from '../data/assets'
 
-const socials = [assets.twitter, assets.instagram, assets.facebook, assets.discord, assets.youtube]
+const socials = [
+  { name: 'Twitter', image: assets.twitter },
+  { name: 'Instagram', image: assets.instagram },
+  { name: 'Facebook', image: assets.facebook },
+  { name: 'Discord', image: assets.discord },
+  { name: 'YouTube', image: assets.youtube },
+]
+
 const payments = [
-  { name: 'BCA', image: assets.payBca, tone: 'blue' },
-  { name: 'BNI', image: assets.payBni, tone: 'white' },
-  { name: 'BRI', image: assets.payBri, tone: 'blue' },
-  { name: 'CIMB', image: assets.payCimb, tone: 'red' },
-  { name: 'Danamon', image: assets.payDanamon, tone: 'white' },
-  { name: 'Maybank', image: assets.payMaybank, tone: 'yellow' },
-  { name: 'Permata', image: assets.payPermata, tone: 'white' },
-  { name: 'DANA', image: assets.payDana, tone: 'blue-light' },
-  { name: 'GoPay', image: assets.payGopay, tone: 'white' },
-  { name: 'LinkAja', image: assets.payLinkaja, tone: 'red' },
-  { name: 'OVO', image: assets.payOvo, tone: 'purple' },
-  { name: 'ShopeePay', image: assets.payShopee, tone: 'orange' },
+  { name: 'BCA', image: assets.payBca },
+  { name: 'BNI', image: assets.payBni },
+  { name: 'BRI', image: assets.payBri },
+  { name: 'CIMB', image: assets.payCimb },
+  { name: 'Danamon', image: assets.payDanamon },
+  { name: 'Maybank', image: assets.payMaybank },
+  { name: 'Permata', image: assets.payPermata },
+  { name: 'DANA', image: assets.payDana },
+  { name: 'GoPay', image: assets.payGopay },
+  { name: 'LinkAja', image: assets.payLinkaja },
+  { name: 'OVO', image: assets.payOvo },
+  { name: 'ShopeePay', image: assets.payShopee },
 ]
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <img className="footer__glow footer__glow--left" src={assets.footerGlow} alt="" />
-      <img className="footer__glow footer__glow--right" src={assets.footerGlow} alt="" />
+    <footer className="footer footer--figma-final">
+      <span className="footer__glow footer__glow--left" aria-hidden="true" />
+      <span className="footer__glow footer__glow--right" aria-hidden="true" />
 
       <div className="footer__content">
         <div className="footer__brand">
           <img className="footer-logo" src={assets.footerLogo || assets.logo} alt="Zetruv" />
-          <p>Your gaming adventure is about to begin, before that let's top up first to make your story easier. Top up the game here, will make you more prepared to face all challenges. Easy transactions, Process only takes a few seconds!</p>
-          <div className="socials">{socials.map((src, index) => <img src={src} alt="" key={src + index} />)}</div>
+          <p>
+            Your gaming adventure is about to begin, before that let's top up first to make your story easier.
+            Top up the game here, will make you more prepared to face all challenges. Easy transactions,
+            Process only takes a few seconds!
+          </p>
+
+          <div className="socials" aria-label="Zetruv social media">
+            {socials.map((social) => (
+              <a href="#" aria-label={social.name} key={social.name}>
+                <img src={social.image} alt="" />
+              </a>
+            ))}
+          </div>
         </div>
 
         <div className="footer__links">
-          <div className="footer__column">
+          <div className="footer__column footer__column--page">
             <h3>Page</h3>
             <a href="/">Homepage</a>
             <a href="#login">Enter</a>
@@ -40,16 +58,28 @@ export default function Footer() {
 
           <div className="footer__support-payment">
             <div className="footer__two-columns">
-              <div className="footer__column"><h3>Support</h3><a href="#youtube">Youtube</a><a href="#facebook">Facebook</a></div>
-              <div className="footer__column"><h3>Legality</h3><a href="#terms">Term &amp; Condition</a></div>
+              <div className="footer__column">
+                <h3>Support</h3>
+                <a href="#">Youtube</a>
+                <a href="#">Facebook</a>
+              </div>
+
+              <div className="footer__column">
+                <h3>Legality</h3>
+                <a href="#terms">Term &amp; Condition</a>
+              </div>
             </div>
+
             <div className="footer__payments">
               <h3>Payment</h3>
-              <div className="payment-grid">
+              <div className="payment-grid" aria-label="Supported payment methods">
                 {payments.map((payment) => (
-                  <span className={`payment-mark payment-mark--${payment.tone}`} key={payment.name} title={payment.name}>
-                    <img src={payment.image} alt={payment.name} />
-                  </span>
+                  <img
+                    className="payment-mark"
+                    src={payment.image}
+                    alt={payment.name}
+                    key={payment.name}
+                  />
                 ))}
               </div>
             </div>
