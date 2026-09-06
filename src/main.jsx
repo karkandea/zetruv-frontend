f60e352e2869c1e6b9aff15a9c280efc54b4677d
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-06 20:50:10 +0700

    style: load final homepage overrides

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles/redesign-a.css'
 import './styles/redesign-b.css'
 import './styles/redesign-c.css'
 import './styles/homepage-top.css'
+import './styles/homepage-final.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
